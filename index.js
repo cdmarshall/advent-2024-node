9b21d8092c0c44f0ef9dc09b3b22f476648493b1
Colton Dean Marshall <cltndmarsh@gmail.com>
2024-12-03 15:28:17 +0000

    Day 3

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function advent01() {
     let column2 = [];
     let result = 0;
 
+    // Create arrays containing each column.
     lines.forEach((line) => {
       let numbers = line.trim().split(/\s+/).map(Number);
       if (numbers.length >= 1) column1.push(numbers[0]);
@@ -24,13 +25,15 @@ function advent01() {
     column1.sort();
     column2.sort();
 
+    // Add difference between each element in each column.
     for (let i = 0; i < column1.length; i++) {
       result += Math.abs(column1[i] - column2[i]);
     }
     console.log("01 - Part 1: " + result);
 
-    // Part 2
     result = 0;
+
+    // Multiply each element in column 1 by the number of occurrences of that element in column 2.
     column1.forEach((number) => {
       result += column2.filter((number2) => number2 === number).length * number;
     });
@@ -50,6 +53,7 @@ function advent02() {
     let safe1 = 0;
     let safe2 = 0;
 
+    // If line is safe, iterate part 1 and 2 counters. If unsafe then check with dampener.
     lines.forEach((line) => {
       let numbers = line.trim().split(/\s+/).map(Number);
       if (checkSafety(numbers)) {
@@ -66,6 +70,7 @@ function advent02() {
   }
 }
 
+// Compare sorted arrays to original to confirm increasing or decreasing. Check diff between each element and next.
 function checkSafety(numbers) {
   let num = JSON.stringify(numbers);
   let sortedasc = JSON.stringify([...numbers].sort((a, b) => a - b));
@@ -84,6 +89,7 @@ function checkSafety(numbers) {
   return true;
 }
 
+// Remove first element, check safety of remaining elements. Iterate through array checking each element in this fashion.
 function problemDampener(numbers) {
   for (let i = 0; i < numbers.length; i++) {
     if (checkSafety([...numbers.slice(0, i), ...numbers.slice(i + 1)]))
@@ -98,7 +104,35 @@ advent03();
 
 function advent03() {
   try {
-    //const lines = processFileLines("input_files/input03.txt");
+    const fileText = fs.readFileSync("input_files/input03.txt", { encoding: "utf8" });
+
+    // Create array of only valid mul statements.
+    let mulStr = fileText.match(/mul\(\d+,\d+\)/g);
+    let result = 0;
+
+    // Multiply each number in each mul statement. Add to total.
+    mulStr.forEach(mul => {
+      let numbers = mul.match(/\d+/g);
+      result += numbers[0] * numbers[1];
+    });
+    console.log("03 - Part 1: " + result);
+
+    // Create array of valid mul and conditional statements
+    let condStr = fileText.match(/mul\(\d+,\d+\)|do\(\)|don't\(\)/g);
+    result = 0;
+    let currentCond = true;
+
+    // Multiply each number in each mul statement following a do statement. Add to total.
+    condStr.forEach(cond => {
+      if(cond.includes("do")) currentCond = true;
+      if(cond.includes("don't")) currentCond = false;
+      if(currentCond && cond.includes("mul")) {
+        let numbers = cond.match(/\d+/g);
+        result += numbers[0] * numbers[1];
+      }
+    });
+    console.log("03 - Part 2: " + result);
+    
   } catch (err) {
     console.error(err);
   }
